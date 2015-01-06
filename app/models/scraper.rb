b9f6b1c89de6e1a4e7df4acf079c79f070ce0aa6
joesiewert <joe.siewert@gmail.com>
2015-01-06 14:05:45 -0700

    Added name cleanup to scraper

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -1,8 +1,10 @@
+require 'open-uri'
+
 class Scraper
 
   def get_kegs
-    url1 = "http://www.hazelsboulder.com/main.asp?request=search&type=B&sel_category=20977&user1=15.5+GALLON&pageNo=1"
-    url2 = "http://www.hazelsboulder.com/main.asp?request=search&type=B&sel_category=20977&user1=15.5+GALLON&pageNo=2"
+    url1 = 'http://www.hazelsboulder.com/main.asp?request=search&type=B&sel_category=20977&user1=15.5+GALLON&pageNo=1'
+    url2 = 'http://www.hazelsboulder.com/main.asp?request=search&type=B&sel_category=20977&user1=15.5+GALLON&pageNo=2'
     page1 = Nokogiri::HTML(open(url1))
     page2 = Nokogiri::HTML(open(url2))
 
@@ -10,8 +12,8 @@ class Scraper
     scrubbed_kegs = []
 
     pages.each do |page|
-      kegs = page.css("a.producttitle")
-      prices = page.css("span.RegularPrice")
+      kegs = page.css('a.producttitle')
+      prices = page.css('span.RegularPrice')
       count = 0
 
       kegs.each do |keg|
@@ -20,7 +22,7 @@ class Scraper
         keg_price = prices[count].text.slice(5..(prices[count].text.length))
 
         scrubbed_keg = {
-          name: keg_name.squish,
+          name: name_cleanup(keg_name.squish),
           size: keg_size.squish,
           price: keg_price
         }
@@ -31,5 +33,45 @@ class Scraper
     end
     scrubbed_kegs
   end
+
+  private
+    def name_cleanup(name)
+      case name
+        when 'Avery Ellies Brown'
+          'Avery Ellie’s Brown Ale'
+        when 'Boulder Buff Gold'
+          'Boulder Buffalo Gold'
+        when 'Coors Original'
+          'Coors Banquet'
+        when 'Deschutes Freshsqueeze IPA'
+          'Deschutes Fresh Squeezed IPA'
+        when 'Deschutes Mirror'
+          'Deschutes Mirror Pond Pale Ale'
+        when 'Guinness Stout'
+          'Guinness'
+        when 'Left Hand Sawtooth'
+          'Left Hand Sawtooth Ale'
+        when 'Odells 5 Barrel'
+          'Odell 5 Barrel Pale Ale'
+        when 'Odells 90 Shilling'
+          'Odell 90 Shilling Ale'
+        when 'Odells Easy Street'
+          'Odell Easy Street Wheat'
+        when 'Odells IPA'
+          'Odell IPA'
+        when 'Odells Levity Ale'
+          'Odell Levity Amber Ale'
+        when 'Oskar Blues Dales Pale'
+          'Oskar Blues Dale’s Pale Ale'
+        when 'Oskar Blues Mama Pils'
+          'Oskar Blues Mama’s Little Yella Pils'
+        when 'Pabst Blue'
+          'Pabst Blue Ribbon'
+        when 'Sierra Nevada Pale'
+          'Sierra Nevada Pale Ale'
+      else
+        name
+      end
+    end
 
 end
