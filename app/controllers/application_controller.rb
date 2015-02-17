a2b237e18c009f199ef09e1df27de0f43719b39b
joesiewert <joe.siewert@gmail.com>
2015-02-17 11:52:08 -0700

    Check for a user session across the site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :ensure_signed_in
   helper_method :current_user
 
   private
@@ -9,4 +10,9 @@ class ApplicationController < ActionController::Base
       User.find_by(id: session[:user_id])
     end
 
+    def ensure_signed_in
+      unless current_user
+        redirect_to signin_path, notice: 'Hey there, you need to sign in first.'
+      end
+    end
 end
