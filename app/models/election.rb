2c4c9df7c6da70db1aec81d93c4fba937de5e4db
joesiewert <joe.siewert@gmail.com>
2015-02-21 21:37:46 -0700

    Added method for counting votes

--- app/models/election.rb
+++ app/models/election.rb
@@ -1,3 +1,24 @@
 class Election < ActiveRecord::Base
+  has_many :votes
+
+  def leading_keg
+    voted_kegs = []
+    votes = Hash.new(0)
+
+    if self.votes.count == 0
+      return "No votes yet"
+    else
+      self.votes.each do |vote|
+        voted_kegs << vote.keg_id
+      end
+
+      voted_kegs.each do |keg|
+        votes[keg] += 1
+      end
+
+      leader = votes.max_by{|keg, votes| votes}
+      return "#{Keg.find(leader[0]).name} (#{leader[1]} votes)"
+    end
+  end
 
 end
